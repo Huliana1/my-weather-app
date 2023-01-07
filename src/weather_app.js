b586a09138377b726125ed26e75ce6ba3119a8be
Huliana1 <116846017+Huliana1@users.noreply.github.com>
2023-01-07 13:39:48 -0500

    Added js for forecast.

--- src/weather_app.js
+++ src/weather_app.js
@@ -69,6 +69,7 @@ function showWeather(response) {
   let windElement = document.querySelector("#kmH");
   humidityElement.innerHTML = response.data.main.humidity + `%`;
   windElement.innerHTML = Math.round(response.data.wind.speed) + `km/h`;
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -101,6 +102,58 @@ currentButton = document.querySelector("#current-button");
 currentButton.addEventListener("click", getCurrentLocation);
 search("Toronto");
 
+//let day = days[date.getDay()];
+//return "${day},${hours},${minutes}";
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function getOshawa() {
   search("Oshawa");
 }
@@ -121,3 +174,4 @@ oshawa.addEventListener("click", getOshawa);
 kitchener.addEventListener("click", getKitchener);
 markham.addEventListener("click", getMarkham);
 brampton.addEventListener("click", getBrampton);
+//displayForecast();
